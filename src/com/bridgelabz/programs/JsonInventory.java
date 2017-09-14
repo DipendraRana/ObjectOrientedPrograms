bbd73ce6c3cc0e16f3a65761a86ad9913b3b353e
DipendraRana <ranadipendra47@gmail.com>
2017-09-14 19:06:37 +0530

    commit

---
+++ src/com/bridgelabz/programs/JsonInventory.java
@@ -0,0 +1,52 @@
+/*********************************************************************
+ * purpose : Create a JSON file having Inventory Details for 
+ * 			 Rice, Pulses and Wheats with properties name, weight, 
+ * 			 price per kg. 
+ *           
+ * @author Dipendra Rana
+ * @version 1.0
+ * @since 14 September 2017          
+ *********************************************************************/
+
+package com.bridgelabz.programs;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.bridgelabz.utility.Utility;
+
+public class JsonInventory {
+	
+	public static Scanner scanner=new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException, ParseException {
+		// TODO Auto-generated method stub 
+		System.out.println("Do you want to Do:");
+		System.out.println("1.Create the JSON file");
+		System.out.println("2.Read from JSON file");
+		int response=scanner.nextInt();
+		if(response==1) {
+			System.out.println("Enter file name to create with absolute address:");
+			String fileName=scanner.nextLine();
+			FileWriter fileWriter=new FileWriter(fileName);
+			JSONObject inventory=Utility.jasonInventory();
+			fileWriter.write(inventory.toJSONString());
+			fileWriter.close();
+			System.out.println("Succesfully Created the Jason file");
+		}
+		else {
+			System.out.println("Enter file name to read with absolute address:");
+			String fileName=scanner.next();
+			JSONParser jasonParser=new JSONParser();
+			JSONObject inventory=(JSONObject) jasonParser.parse(new FileReader(fileName));
+			Utility.inventoryItemValueCalculation(inventory);
+		}
+	}
+
+}
--- src/com/bridgelabz/utility/Utility.java
+++ src/com/bridgelabz/utility/Utility.java
@@ -1,7 +1,18 @@
+/*********************************************************************
+ * purpose : Object Oriented programs Logic
+ *           
+ * @author Dipendra Rana
+ * @version 1.0
+ * @since 14 September 2017          
+ *********************************************************************/
+
 package com.bridgelabz.utility;
 
 import java.util.Collections;
 import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class Utility {
 	
@@ -30,5 +41,85 @@ public class Utility {
 		Collections.sort(queue.queue);
 		queue.print();
 		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static JSONObject jasonInventory() {
+		JSONObject inventory=new JSONObject();
+		JSONObject basmati=new JSONObject();	//basmati rice object
+		basmati.put("Name","Basmati");
+		basmati.put("Weight", new Double(300));
+		basmati.put("Price", new Double(120));
+		JSONObject mysoori=new JSONObject();
+		mysoori.put("Name", "Mysoori");	//myssori rice object
+		mysoori.put("Weight", new Double(450));
+		mysoori.put("Price", new Double(65));
+		JSONArray rice=new JSONArray();	//basmati and myssori object is taken under JsonArray 
+		rice.add(basmati);
+		rice.add(mysoori);
+		inventory.put("Rice", rice); //rice array is added as value with key as rice to inventory
+		JSONObject kidneyBeans=new JSONObject();
+		kidneyBeans.put("Name", "Kidney Beans");
+		kidneyBeans.put("Weight", new Double(160));
+		kidneyBeans.put("Price", new Double(75));
+		JSONObject chickPeas=new JSONObject();
+		chickPeas.put("Name", "Chick Peas");
+		chickPeas.put("Weight", new Double(250));
+		chickPeas.put("Price", new Double(85));
+		JSONArray pulse=new JSONArray();
+		pulse.add(kidneyBeans);
+		pulse.add(chickPeas);
+		inventory.put("Pulse", pulse);
+		JSONObject wholeGrain=new JSONObject();
+		wholeGrain.put("Name", "Whole Grain");
+		wholeGrain.put("Weight", new Double(400));
+		wholeGrain.put("Price", new Double(66));
+		JSONObject plain=new JSONObject();
+		plain.put("Name", "Plain");
+		plain.put("Weight", new Double(600));
+		plain.put("Price", new Double(35));
+		JSONArray wheat=new JSONArray();
+		wheat.add(wholeGrain);
+		wheat.add(plain);
+		inventory.put("Wheat", wheat);
+		return inventory;
+	}
+
+	public static void inventoryItemValueCalculation(JSONObject inventory) {
+		JSONArray rice=(JSONArray)inventory.get("Rice");
+		JSONObject basmati=(JSONObject)rice.get(0);
+		Double weight=(Double) basmati.get("Weight");
+		Double price=(Double) basmati.get("Price");
+		double valueOfBasmatiRice=weight.doubleValue()*price.doubleValue();
+		System.out.println("The total value of basmati rice present is(Rs) "+valueOfBasmatiRice);
+		JSONObject mysoori=(JSONObject)rice.get(1);
+		weight=(Double) mysoori.get("Weight");
+		price=(Double) mysoori.get("Price");
+		double valueOfMysooriRice=weight.doubleValue()*price.doubleValue();
+		System.out.println("The total value of basmati rice present is(Rs) "+valueOfMysooriRice);
+		/***********************************************/
+		JSONArray pulse=(JSONArray) inventory.get("Pulse");
+		JSONObject kidneyBeans=(JSONObject)pulse.get(0);
+		weight=(Double) kidneyBeans.get("Weight");
+		price=(Double) kidneyBeans.get("Price");
+		double valueOfKidneyBeans=weight.doubleValue()*price.doubleValue();
+		System.out.println("The total value of Kidney Beans present is(Rs) "+valueOfKidneyBeans);
+		JSONObject chickPeas=(JSONObject)rice.get(1);
+		weight=(Double) chickPeas.get("Weight");
+		price=(Double) chickPeas.get("Price");
+		double valueOfChickPeas=weight.doubleValue()*price.doubleValue();
+		System.out.println("The total value of Chik peas present is(Rs) "+valueOfChickPeas);
+		/************************************************/
+		JSONArray wheat=(JSONArray) inventory.get("Wheat");
+		JSONObject wholeGrain=(JSONObject)wheat.get(0);
+		weight=(Double) wholeGrain.get("Weight");
+		price=(Double) wholeGrain.get("Price");
+		double valueOfWholeGrain=weight.doubleValue()*price.doubleValue();
+		System.out.println("The total value of Whole grain wheat present is(Rs) "+valueOfWholeGrain);
+		JSONObject plain=(JSONObject)rice.get(1);
+		weight=(Double) plain.get("Weight");
+		price=(Double) plain.get("Price");
+		double valueOfPlainWheat=weight.doubleValue()*price.doubleValue();
+		System.out.println("The total value of Plain Wheat present is(Rs) "+valueOfPlainWheat);
 	}
 }
